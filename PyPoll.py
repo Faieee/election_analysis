3f1cb9eb5334cefc24993a8c9561c1e19ec0e2c1
Anton <anton.z.99@gmail.com>
2022-06-19 17:54:56 -0700

    election results code finished

--- PyPoll.py
+++ PyPoll.py
@@ -7,19 +7,45 @@
 import csv
 import os
 
-# file paths
-openFilePath = os.path.join("resources", "election_results.csv")
+openFilePath = os.path.join("resources", "election_results.csv")        # set file paths
 writeFilePath = os.path.join("analysis", "election_analysis.txt")
 
-# read analysis file
-with open(openFilePath) as electionData:
-    fileReader = csv.reader(electionData)                   # initialize reader
+candidateList = []                                                      # track things we care about
+candidateVotes = {}
+totalVotes = 0
 
-    header = next(fileReader)                               # print header row
+with open(openFilePath) as electionData:                                # read analysis file
+    fileReader = csv.reader(electionData)                               # initialize reader
+
+    header = next(fileReader)                                           # print header row
     print(header)
 
-# write analysis file
-with open(writeFilePath, "w") as electionAnalysis:
-    electionAnalysis.write("Counties in the Election\n------------------\n")
-    electionAnalysis.write("Arapahoe\nDenver\nJefferson")
+    for row in fileReader:
+        totalVotes += 1
 
+        candidateName = row[2]                                          # find candidate names
+        if candidateName not in candidateList:
+            candidateList.append(candidateName)
+            candidateVotes[candidateName] = 0                           # start tracking candidate's votes
+
+        candidateVotes[candidateName] += 1
+
+    print(candidateVotes)
+
+winningCandidate = ""
+winningCount = 0
+winningPercentage = 0
+for candidateName in candidateVotes:                                    # find vote percentages and winner
+    votes = candidateVotes[candidateName]
+    votePercentage = float(votes) / float(totalVotes) * 100
+    print(f"{candidateName} received {votePercentage:.1f}% of the votes.")
+
+    if candidateVotes[candidateName] > winningCount:
+        winningCandidate = candidateName
+        winningCount = votes
+        winningPercentage = votePercentage
+
+print(f"\nWinner is {winningCandidate} with {winningPercentage:.1f}% of the votes ({winningCount})")
+
+with open(writeFilePath, "w") as electionAnalysis:                      # write analysis file
+    exit(0)
